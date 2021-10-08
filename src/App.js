051b7ed01bbd0d9ae92892f842f91f6d35a628c0
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-10-08 14:45:49 +0200

    feat: Initial Architecture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
+import { Header, List, Map } from './components';
 
 function App() {
   return (
-    <div>
-      <h1>zZZzZzzzz</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
